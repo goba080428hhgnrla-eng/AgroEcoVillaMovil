b063c67a95bf56b2432bf0c9b5675b37c4440c84
Martinez <reynaldomartinezbaltazar864@gmail.com>
2026-05-26 14:30:14 -0600

    correcion e colores rey

--- app/src/main/java/com/example/agroconectavilla/utils.kt
+++ app/src/main/java/com/example/agroconectavilla/utils.kt
@@ -1,7 +1,6 @@
 package com.example.agroconectavilla.utils
 
 object Constants {
-    //const val BASE_URL = "https://ritalin-detonator-womb.ngrok-free.dev/"
-    const val BASE_URL = "https://agroconectavilla.onrender.com/"
+    const val BASE_URL = "https://ritalin-detonator-womb.ngrok-free.dev/"
     //
 }
