61327bf630846785796e97b0123b2b661d807e03
saida <goba080428hhgnrla0@soycecytem.mx>
2026-05-26 13:13:35 -0600

    Actualización de alan 5

--- app/src/main/java/com/example/agroconectavilla/utils.kt
+++ app/src/main/java/com/example/agroconectavilla/utils.kt
@@ -3,4 +3,5 @@ package com.example.agroconectavilla.utils
 object Constants {
     //const val BASE_URL = "https://ritalin-detonator-womb.ngrok-free.dev/"
     const val BASE_URL = "https://agroconectavilla.onrender.com/"
+    //
 }
